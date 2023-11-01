90f6953e4b0271de37cdbf11b3143fe396a53e2a
MatBuompy <matteo.buompastore088@gmail.com>
2023-11-01 13:37:12 +0100

    Added actor mapper and datasoruce implementation

---
+++ lib/infrastructure/datasources/actor_moviedb_datasource_impl.dart
@@ -0,0 +1,29 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/actors_datasource.dart';
+import 'package:cinemapedia/domain/models/actor.dart';
+import 'package:cinemapedia/infrastructure/mappers/actor_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
+import 'package:dio/dio.dart';
+
+class ActorMovieDbDataSource extends ActorsDataSource {
+  final _dio = Dio(
+      BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api_key': Environment.movieDbKey,
+    'language': 'en-US',
+  }));
+
+  List<Actor> _jsonToActors(Map<String, dynamic> json) {
+    final actorDbResponse = CreditsResponse.fromJson(json);
+    final List<Actor> actors = actorDbResponse.cast
+        .map((actor) => ActorMapper.castDbToEntity(actor))
+        .toList();
+    return actors;
+  }
+
+  @override
+  Future<List<Actor>> getActorsByMovie({required String movieId}) async {
+    final response = await _dio.get('/movie/$movieId/credits');
+    final actors = _jsonToActors(response.data);
+    return actors;
+  }
+}
