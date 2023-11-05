a2c107180d5462f7ef03de5ed82f20579a566402
MatBuompy <matteo.buompastore088@gmail.com>
2023-11-05 16:13:25 +0100

    Isar instance created in datasource implementation

--- lib/infrastructure/datasources/favorite_local_storage_datasource_impl.dart
+++ lib/infrastructure/datasources/favorite_local_storage_datasource_impl.dart
@@ -1,8 +1,23 @@
-
 import 'package:cinemapedia/domain/datasources/favorites_local_storage_datasource.dart';
 import 'package:cinemapedia/domain/models/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
-class FavoriteLocalStorageDataSourceImpl extends FavoritesLocalStorageDataSource {
+class FavoriteLocalStorageDataSourceImpl
+    extends FavoritesLocalStorageDataSource {
+  late Future<Isar> db;
+
+  FavoriteLocalStorageDataSourceImpl() {
+    db = _openDB();
+  }
+
+  Future<Isar> _openDB() async {
+    final appDirectory = await getApplicationCacheDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema], directory: appDirectory.path);
+    }
+    return Future.value(Isar.getInstance());
+  }
 
   @override
   Future<bool> isMovieFavorite(int movieId) {
@@ -21,5 +36,4 @@ class FavoriteLocalStorageDataSourceImpl extends FavoritesLocalStorageDataSource
     // TODO: implement toggleFavorite
     throw UnimplementedError();
   }
-
 }
