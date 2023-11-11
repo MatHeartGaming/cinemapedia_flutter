12492dbedbabaaeec4cbb135b2897c94e2cec1d7
MatBuompy <matteo.buompastore088@gmail.com>
2023-11-11 14:43:32 +0100

    Favorites completed

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,7 +1,9 @@
+import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -39,9 +41,33 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
-    return Scaffold(body: MovieMasonry(
-      movies: favoriteMovies,
-      loadNextPage: _loadNextPage,
+    final colors = Theme.of(context).colorScheme;
+    return Scaffold(
+        body: Visibility(
+      visible: favoriteMovies.isNotEmpty,
+      replacement: _emptyFavoritesWidget(colors),
+      child: MovieMasonry(
+        movies: favoriteMovies,
+        loadNextPage: _loadNextPage,
+      ),
     ));
+  }
+
+  Widget _emptyFavoritesWidget(ColorScheme colors) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.favorite_outline_rounded, size: 60, color: colors.primary,),
+          Text("Ooh No!", style: TextStyle(fontSize: 20, color: colors.primary),),
+          const Text("No tienes peliuclas favoritas 😔", style: TextStyle(fontSize: 20, color: Colors.black45),),
+          const SizedBox(height: 20,),
+          FilledButton.tonal(
+            onPressed: () => context.go(basePath), 
+            child: const Text("Añade una peli ♥︎")),
+        ],
+      ),
+    );
   }
 }
